ba50e51ccfee004dab75e8baf789ce16842e3700
Vo Van Hung <vovanhung2864@gmail.com>
2024-10-17 01:11:20 +0700

    Hung has completed home_page

--- src/app/features/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule, FooterComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
